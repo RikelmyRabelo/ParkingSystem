8040499ace7eb6ad7ab00d0118272c99ef6fb877
Rikelmy Rabelo <rikelmyrabeo2@gmail.com>
2025-02-09 18:23:38 -0300

    fix(horas): as horas adicionadas não mudam mais as passadas; feat(horas): as horas agora aparecem também na parte de retirar um veiculo

--- SistemaEstacionamento/EstacionamentoSistema/Models/Estacionamento.cs
+++ SistemaEstacionamento/EstacionamentoSistema/Models/Estacionamento.cs
@@ -6,7 +6,7 @@ namespace SistemaEstacionamento.Models
         private decimal precoPorHora = 0;
         private int capacidade = 0;
 
-        private List<string> veiculos = new List<string>();
+        private List<Veiculo> veiculos = new List<Veiculo>();
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora, int capacidade)
         {
@@ -25,7 +25,7 @@ namespace SistemaEstacionamento.Models
 
             Console.WriteLine("Digite a placa do veículo para estacionar:");
             string placa = Console.ReadLine();
-            veiculos.Add(placa.ToUpper());
+            veiculos.Add(new Veiculo(placa.ToUpper()));
         }
 
         public void RemoverVeiculo()
@@ -34,9 +34,9 @@ namespace SistemaEstacionamento.Models
             if (veiculos.Any())
             {
                 Console.WriteLine("Carros cadastrados:");
-                foreach (var veiculo in veiculos)
+                foreach (var Veiculo in veiculos)
                 {
-                    Console.WriteLine(veiculo);
+                    Console.WriteLine($"Placa: {Veiculo.Placa} - Hora de Entrada: {Veiculo.HoraEntrada.ToString("HH:mm:ss")}");
                 }
 
                 // Solicita a placa do veículo a ser removido
@@ -44,24 +44,24 @@ namespace SistemaEstacionamento.Models
                 string placa = Console.ReadLine();
 
                 // Verifica se o veículo existe
-                if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+                if (veiculos.Any(x => x.Placa.ToUpper() == placa.ToUpper()))
                 {
                     Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
                     int horas = int.Parse(Console.ReadLine());
-                        if (horas <= 1)
-                        {
-                            Console.WriteLine("Você ficou apenas uma hora ou menos, por isso recebeu R$5 de desconto no valor total.");
-                            decimal valorTotal = precoInicial + precoPorHora * horas - 5 ;
-                            Console.WriteLine($"O veículo {placa.ToUpper()} foi removido e o preço total foi de: R$ {valorTotal}");
-                        }
-                        else
-                        {
-                            decimal valorTotal = precoInicial + precoPorHora * horas;
-                            Console.WriteLine($"O veículo {placa.ToUpper()} foi removido e o preço total foi de: R$ {valorTotal}");
-                        }
+                    if (horas <= 1)
+                    {
+                        Console.WriteLine("Você ficou apenas uma hora ou menos, por isso recebeu R$5 de desconto no valor total.");
+                        decimal valorTotal = precoInicial + precoPorHora * horas - 5;
+                        Console.WriteLine($"O veículo {placa.ToUpper()} foi removido e o preço total foi de: R$ {valorTotal}");
+                    }
+                    else
+                    {
+                        decimal valorTotal = precoInicial + precoPorHora * horas;
+                        Console.WriteLine($"O veículo {placa.ToUpper()} foi removido e o preço total foi de: R$ {valorTotal}");
+                    }
 
                     // Remove o veículo da lista
-                    veiculos.Remove(placa.ToUpper());
+                    veiculos.Remove(veiculos.First(x => x.Placa.ToUpper() == placa.ToUpper()));
                 }
                 else
                 {
@@ -80,12 +80,11 @@ namespace SistemaEstacionamento.Models
             // Verifica se há veículos no estacionamento
             if (veiculos.Any())
             {
-                DateTime horaEntrada = DateTime.Now;
                 Console.WriteLine("Os veículos estacionados são:");
                 foreach (var veiculo in veiculos)
                 {
-                    
-                    Console.WriteLine($"{veiculo} - Hora de Entrada: {horaEntrada.ToString("HH:mm:ss")}");
+                    Console.WriteLine($"Placa: {veiculo.Placa} - Hora de Entrada: {veiculo.HoraEntrada.ToString("HH:mm:ss")}");
+
                 }
             }
             else
@@ -105,7 +104,18 @@ namespace SistemaEstacionamento.Models
                 Console.WriteLine("O estacionamento está cheio!⚠️");
             }
 
+        }
 
+        public class Veiculo
+        {
+            public string Placa { get; set; }
+            public DateTime HoraEntrada { get; set; }
+
+            public Veiculo(string placa)
+            {
+                Placa = placa;
+                HoraEntrada = DateTime.Now;
+            }
         }
     }
 }
